b108955f0ccc6df07ef9779780eea1100cc364d2
Marc Le Masson <sylaratomic@gmail.com>
2021-01-07 18:49:11 +0000

    Website Update - New Style and Data
    + moved to a new style
    + added lots of new data
    + moved old design into "old" folder

--- public/old/js/scrolling-nav.js
+++ public/old/js/scrolling-nav.js
@@ -16,9 +16,14 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  /*$('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
-  });
+  });*/
+
+  /*$('.js-scroll-trigger').click(function(){
+    $('#welcome').addClass("panel");
+
+  })*/
 
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
